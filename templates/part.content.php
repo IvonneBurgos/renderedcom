78e25c3022607068188f754fbc7e942fbf0e65ae
IvonneBurgos <irburgos@espol.edu.ec>
2016-06-13 15:39:34 -0500

    Ejemplos de cosas que sirven para crear carpeta en el Local Storage. Pero no funciona

--- templates/part.content.php
+++ templates/part.content.php
@@ -23,10 +23,81 @@
     </form>
           
     <?php
-     
-        $last= shell_exec('sh /opt/cgru/setup3.sh; python /opt/cgru/afanasy/python/job.py');
-        echo "Command returned $last" ;
-    
+         
+          
+        $username = OC_User::getUser(); 
+        $datadir=OC\Files\Filesystem::getLocalFolder('Nube Multimedia');
+        $var2=OC\Files\Storage\Local::mkdir('var/www/owncloud/Nube Multimedia/new');
+        /*$array = ["datadir" => Nube Multimedia\/];  
+        $var = new OC\Files\Storage\Local;
+        $var::__construct($array);*/
+        
+      
+          
+
+        //OC\Files\Storage\Local::$datadir;
+
+        // $path=OC\Files\Storage\Local
+
+        //$var3=OC\Files\Storage\Local::mkdir('newFolder');
+
+        //echo $var2;
+
+        //$var2=OC\Files\Filesystem::mkdir($username);
+        //$arguments  = array("datadir" => $var);
+        //$var2=OC\Files\Storage\Local::__construct($arguments);
+        // $var2=OC\Files\Storage\Local::mkdir('shared/Jobs/new');
+        //$var2=OC\Files\Storage\Local::getSourcePath($var);
+        //$var2=OCP\Files\Folder::nodeExists('Documents');
+        //$var2=OC\Files\Storage\Local::is_dir($var);
+        //$var3=OC\Files\Filesystem::getId();
+        //echo $var2;
+        //echo $var3;
+        //OC\Files\Filesystem::mkdir($var);
+        // $data= array('usuario'=>$username,'escena'=>'A whole new level','frame_ini'=> 1,'frame_fin'=> 10);
+        // $result= shell_exec('sh /opt/cgru/setup3.sh; python "/opt/cgru/afanasy/python/job.py" ' . escapeshellarg(json_encode($data)));
+        // echo $result;
+        // Decode the result
+        // $resultData = json_decode($result, true);
+        // This will contain: array('status' => 'Yes!')
+        //  var_dump($resultData);
+        //Para crear una nueva carpeta
+        //OC\Files\Filesystem::mkdir('newFolder');
+        //echo $var . 'new3' ;
+        //------------------------------------------------------------------------------------------------//
+        //Cosas que no valen pero las dejo por si algun dias me sirven
+        /* $structure = "./" . $var;
+        echo $structure ;*/
+        /*if (!mkdir("var/www/shared/Jobs/User1", 0777, true)) {
+        die('Failed to create folders...');
+        }*/
+        //$path=OC\Files\Filesystem::getInternalPath('nueva1');
+        //$intento=OC\Files\Filesystem::getMountPoint('coolForTheSummer');
+        //echo 'PATH= '. $path . 'OK';
+        //echo $intento;
+
+        // getStorage() : \OCP\Files\Storage;
+        //https://doc.owncloud.org/api/classes/OCP.Files.FileInfo.html#method_getStorage
+        //https://doc.owncloud.org/api/classes/OCP.Files.Storage.html#method_mkdir
+        // echo getPath($newFolder) ;
+
+        /* OC\Files\Filesystem::mkdir( 'so' );
+        $username = OC_User::getUser(); 
+        $data= array('escena'=>'New Job','frame_ini'=> 1,'frame_fin'=> 10);
+        $result= shell_exec('sh /opt/cgru/setup3.sh; python "/opt/cgru/afanasy/python/job.py" ' . escapeshellarg(json_encode($data)));
+        echo $result;
+        // Decode the result
+        $resultData = json_decode($result, true);
+        // This will contain: array('status' => 'Yes!')
+        var_dump($resultData);
+        /* $file = str_replace(array('/', '\\'), '',  $_GET['so']);
+        fopen("/data/" . $username . "/" . $file . ".txt");*/
+        // echo $username;
+        /*$file = str_replace(array('/', '\\'), '',  $_GET['file']);
+        fopen("/data/" . $username . "/" . $file . ".txt");*/
+        //echo getStorage() : \OCP\Files\Filesystem;
+        //https://doc.owncloud.org/api/classes/OCP.Files.FileInfo.html#method_getStorage
+        // echo getPath( OC\Files\Filesystem\So_new2) ;
     ?>
       </div>
     </div>
