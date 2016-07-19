93ef055fcb913a90eb57b9dabc83f386ed54f850
IvonneBurgos <irburgos@espol.edu.ec>
2016-07-19 16:19:37 -0500

    Funciones JS para la verificación de ingreso correcto de números y no campos vacíos

--- js/script.js
+++ js/script.js
@@ -8,8 +8,25 @@
  * @copyright IvonneBurgos 2016
  */
 (function ($, OC) {
-    function checkFields(){
-        var camposTexto = document.getElementsByTagName('input');
+    function checkEmptyFields(input_elements){
+        var ban = true;
+        var camposTexto = input_elements;
+        for(var i = 0; i < camposTexto.length; i++){
+            var value = $(camposTexto[i]).val().length;
+            if (value == 0){
+                ban = false;
+                alert('ese campo esta vacio');
+            } }     return ban; }
+
+    function checkNumberRange(val1,val2){
+        var valu1= parseInt(val1);
+        var valu2= parseInt(val2);
+        var ban = true;
+        if (valu1 > valu2){
+            alert('El frame de inicio debe ser menor que el frame final');
+            ban= false;
+        }
+       return ban; 
     }
     
 	$(document).ready(function () {
@@ -31,13 +48,11 @@
         
         
 		$('#render').click(function(){
-          checkFields();
-           /* checkedFields = checkFields($('#scene').val(),$('#frame_ini').val(), $('#frame_fin').val());
-
-            if (checkedFields == false){
-                alert ("Hay campos sin llenar");
-            }
-            else {   }*/
+            
+         var banField = checkEmptyFields(document.getElementById('form').getElementsByTagName('input'));
+         var banNumber = checkNumberRange($('#frame_ini').val(),$('#frame_fin').val());
+            if (banField == true ){
+                alert ('Baia baia si llenaste todos los campos');
                 /*var url = OC.generateUrl('/apps/renderedcom/job');
                 var data = {
                 scene: $('#scene').val(),
@@ -47,7 +62,19 @@
                 $.post(url, data).success(function (response) {
                 $('#echo-result').text(response);
                 });*/
-         
+            }
+              if (banNumber == true ){
+                alert ('El rango de numeros es correcto');
+                /*var url = OC.generateUrl('/apps/renderedcom/job');
+                var data = {
+                scene: $('#scene').val(),
+                frame_ini: $('#frame_ini').val(),
+                frame_fin: $('#frame_fin').val()
+                };
+                $.post(url, data).success(function (response) {
+                $('#echo-result').text(response);
+                });*/
+            }
 		});
 	});
     
