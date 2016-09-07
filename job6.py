88ccfb164bac194b880d6dbdd8cd9e32c1c63cd2
IvonneBurgos <irburgos@espol.edu.ec>
2016-09-07 16:16:51 -0500

    Agregar el ejemplo de la oficina del trabajo

---
+++ job6.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# Import afanasy python module ( must be in PYTHONPATH)
+import af, sys, json
+
+# Load the data that PHP sent us
+try:
+    data = json.loads(sys.argv[1])
+except:
+    print "ERROR"
+    sys.exit(1)
+
+
+# Generate some data to send to PHP
+result = {'status': 'Yes!'}
+#folder = "/var/www/owncloud/" + data["folder"]
+
+# Create a job
+job = af.Job(data["scene"])
+
+job.setUserName(data["user"])
+# Set maximum tasks that can be executed simultaneously
+job.setMaxRunningTasks( 5)
+
+# Create a block with provided name and service type
+block = af.Block('blenderRender', 'blender')
+
+# Set block tasks working directory
+block.setWorkingDirectory('/var/www/owncloud/data/'+data["user"]+'/files/Documents')
+
+# Set block tasks command
+block.setCommand('blender -b \"/var/www/owncloud/data/'+data["user"]+'/files/Documents/scene.blend\" -o \"/var/www/owncloud/Nube_Multimedia/'+data["user"]+'/'+data["scene"]+'/image\" -s @#@ -e @#@ -j 1 -a')
+
+# Set block tasks preview command arguments
+block.setFiles(["/var/www/owncloud/Nube_Multimedia/"+data["user"]+"/"+data["scene"]+"/image"])
+
+# Set block to numeric type, providing first, last frame and frames per host
+block.setNumeric( data["frame_ini"], data["frame_fin"], 1)
+
+# Add block to the job
+job.blocks.append( block)
+
+# Send job to Afanasy server
+job.send()
+
+#Imprimir un mensaje
+print 'Trabajo renderizando'
+
+# Send it to stdout (to PHP)
+#print json.dumps(data)
+
