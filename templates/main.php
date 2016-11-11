d5f0be5c9b1488567dd4c7d0ab01e38a040134cf
Ronny Valdivieso <ron_251@hotmail.com>
2016-11-11 12:06:56 -0500

    estilo formulario

--- templates/main.php
+++ templates/main.php
@@ -6,6 +6,10 @@ style('renderedcom', array('bootstrap', 'font-awesome.min','style'));
 <div id="app" >
 	<div id="app-content" class="row" >
 		<div id="app-content-wrapper" class="col-xs-12 col-sm-12 col-md-12">
+            <div id="job-nav" class="col-xs-12 col-sm-2 col-md-2 sidenav">
+                <p id="userNameFront" class="bg-success"><?php p($_['user']);?></p>
+                <?php print_unescaped($this->inc('part.navigation')); ?>
+            </div>
             <div class="col-xs-12 col-sm-6 col-md-6 render-form">
                 <?php print_unescaped($this->inc('content')); ?>
             </div>
