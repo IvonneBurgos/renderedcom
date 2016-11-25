6304fd557737c154f231cd6f27cbd70171059f81
Ronny Valdivieso <ron_251@hotmail.com>
2016-11-25 00:53:57 -0500

    nav

--- templates/part.navigation.php
+++ templates/part.navigation.php
@@ -1,5 +1,5 @@
 <ul>
-	<!-- <li><a id="endJob" href="#">Trabajos Finalizados</a></li> -->
-	<li><a id="processJob" href="/owncloud/index.php/apps/mistrabajos/">Trabajos</a></li>
-	<li><a id="render-edcom" href="/owncloud/index.php/apps/renderedcom/">Render Edcom</a></li>
+	<li><a id="endJob" href="#">Trabajos Finalizados</a></li>
+	<li><a id="processJob" href="#">Trabajos en Proceso</a></li>
+	<li><a id="render-edcom" class="selectedOption" href="/owncloud/index.php/apps/renderedcom/">Render Edcom</a></li>
 </ul>
