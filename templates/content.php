efae8d0c52750a98971c1c0674c043e82f21579d
Ronny Valdivieso <ron_251@hotmail.com>
2018-01-18 05:09:19 -0500

    Prototipo

--- templates/content.php
+++ templates/content.php
@@ -1,46 +1,49 @@
-
 <div id="form">
-    <div class="row">
-        <div class="col-md-2">
-            <label class='labelPosition'>Nombre de Escena: </label>
-        </div>
-        <div class="col-md-8">
-            <input id="scene" name="scene" class="formElements lower" type="text" placeholder="mi_render">
-        </div>
-        </div>
-    <div class="row">
-        <div class="col-md-2">
-            <label class='labelPosition'>Archivo: </label>
-        </div>
-        <div class="col-md-6" >
-            <input id="file_path" name="file_path" class="formElements" type="text" placeholder="mi_archivo.xy" readonly>
-        </div>
-        <div class="col-md-1"></div>
-        <div class="col-md-1" >
-           <button id="buscar" class="btn btn-primary formElements">Buscar</button>
-        </div>
-        <div id="progress-div" class="col-xs-12 col-sm-6 col-md-6 progress">
-            <?php print_unescaped($this->inc('progreso')); ?>
-        </div>
-    </div>
-    <div class="row">
-        <div  class="col-md-2">
+   <div class="row">
+      <div class="col-md-12">
+         <label class='labelPosition'>Nombre de Escena: </label>
+      </div>
+      <div class="col-md-12">
+         <input id="scene" name="scene" class="formElements lower" type="text" placeholder="mi_render">
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md-12">
+         <label class='labelPosition'>Archivo: </label>
+      </div>
+      <div class="col-sm-8 col-md-8" >
+         <input id="file_path" name="file_path" class="formElements" type="text" placeholder="mi_archivo.xy" readonly>
+      </div>
+      <div class="col-sm-1 col-md-1"></div>
+      <div class="col-sm-3 col-md-3" >
+         <button id="buscar" class="btn btn-primary formElements">Buscar</button>
+      </div>
+      <div id="progress-div" class="col-xs-12 col-sm-12 col-md-12 progress">
+         <?php print_unescaped($this->inc('progreso')); ?>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md-6">
+         <div  class="col-md-12">
             <label class='labelPosition'>Frames desde: </label>
-        </div>
-        <div id="div_ini" class="col-md-3" >
+         </div>
+         <div id="div_ini" class="col-md-12" >
             <input id="frame_ini" name="frame_ini" class="formElements" type="text" placeholder="1">
-        </div>
-        <div class="col-md-2">
+         </div>
+      </div>
+      <div class="col-md-6">
+         <div class="col-md-12">
             <label class='labelPosition' id='label_frame_fin'>hasta: </label>
-        </div>
-        <div class="col-md-3">
+         </div>
+         <div class="col-md-12">
             <input id="frame_fin" name="frame_fin" class="formElements" type="text" placeholder="99" >
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-2">
-            <button id="render" type="submit" class="btn btn-success">Renderizar</button>
-        </div>
-    </div>
-    <div id="echo-result"></div>
+         </div>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-md-12">
+         <button id="render" type="submit" class="btn btn-success">Renderizar</button>
+      </div>
+   </div>
+   <div id="echo-result"></div>
 </div>
--- templates/main.php
+++ templates/main.php
@@ -6,13 +6,16 @@ style('renderedcom', array('bootstrap', 'font-awesome.min','style'));
 <div id="app" >
 	<div id="app-content" class="row" >
 		<div id="app-content-wrapper" class="col-xs-12 col-sm-12 col-md-12">
-            <div id="job-nav" class="col-xs-12 col-sm-2 col-md-2 sidenav-left">
-                <p id="userNameFront" class="bg-success"><?php p($_['user']);?></p>
-                <?php print_unescaped($this->inc('part.navigation')); ?>
-            </div>
-            <div class="col-xs-12 col-sm-10 col-md-10 render-form">
-                <?php print_unescaped($this->inc('content')); ?>
-            </div>
+         <div id="job-nav" class="sidenav-left">
+				<div class="user-image">
+					<i class="fa fa-user-circle" aria-hidden="true"></i>
+				</div>
+				<p id="userNameFront"><?php p($_['user']);?></p>
+				<?php print_unescaped($this->inc('part.navigation')); ?>
+         </div>
+         <div class="render-form">
+         	<?php print_unescaped($this->inc('content')); ?>
+         </div>
 		</div>
 	</div>
 </div>
